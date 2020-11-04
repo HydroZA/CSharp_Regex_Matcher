cb50c1a20839ed0c7fbda9447904caaa9e69159f
HydroZA <jmslegge0@gmail.com>
2020-11-04 16:12:18 +0000

    Initial Commit
    Initial Commit

---
+++ Matcher.cs
@@ -0,0 +1,145 @@
+﻿
+
+using System.Collections;
+using System.Linq;
+
+namespace cw1
+{
+    public class Matcher
+    {
+        public bool nullable(Rexp r) => r switch
+        { 
+            ZERO _ => false,
+            ONE _ => true,
+            CHAR _ => false,
+            ALT alt => nullable(alt.r1) || nullable(alt.r2),
+            SEQ seq => nullable(seq.r1) && nullable(seq.r2),
+            STAR _ => true,
+            RANGE _ => false,
+            PLUS pl => nullable(pl.r),
+            OPTIONAL _ => true,
+            NTIMES nt => nt.n == 0 || nullable(nt.r),
+            UPTO _ => true,
+            FROM frm => frm.n == 0 || nullable(frm.r),
+            BETWEEN btw => btw.n == 0 || nullable(btw.r),
+            NOT not => !(nullable(not.r)),
+            CFUN _ => false,
+            ALL => false
+        };
+
+        public Rexp der(char c, Rexp r) => r switch
+        {
+            ZERO _ => r,
+            ONE _ => new ZERO(),
+            CHAR ch => der(c, new CFUN(ch.c.ToString().ToHashSet())),
+            //CHAR ch => (c == ch.c) ? (Rexp)new ONE() : new ZERO(),
+            ALT alt => new ALT(der(c, alt.r1), der(c, alt.r2)),
+            SEQ seq => (nullable(seq.r1)) switch
+            {
+                true => new ALT(new SEQ(der(c, seq.r1), seq.r2), der(c, seq.r2)),
+                false => new SEQ(der(c, seq.r1), seq.r2)
+            },
+            STAR star => new SEQ(der(c, star.r), new STAR(star.r)),
+            RANGE rng => der(c, new CFUN(rng.s)),
+            PLUS plus => new SEQ(der(c, plus.r), new STAR(plus.r)),
+            OPTIONAL opt => der(c, opt.r),
+            NTIMES nt => nt.n == 0 ? (Rexp) new ZERO() : new SEQ(der(c, nt.r), new NTIMES(nt.r, nt.n - 1)),
+            UPTO upto => upto.m switch
+            {
+                0 => new ZERO(),
+                _ => new SEQ(der(c, upto.r), new UPTO(upto.r, upto.m - 1))
+            },
+            FROM frm => frm.n == 0 ? new SEQ(der(c, frm.r), new STAR(r)) : new SEQ(der(c, frm.r), new FROM(frm.r, frm.n - 1)),
+            BETWEEN btwn => (btwn.r, btwn.n, btwn.m) switch
+            {
+                (_, _, 0) => new ZERO(),
+                (_, _, 1) => der(c, btwn.r),
+                (_, 0, _) => new SEQ(der(c, btwn.r), new BETWEEN(btwn.r, 0, btwn.m - 1)),
+                (_, _, _) => new SEQ(der(c, btwn.r), new BETWEEN(btwn.r, btwn.n - 1, btwn.m - 1))
+            },
+            NOT not => new NOT(der(c, not.r)),
+            //CFUN cf => cf.f(c) ? new ONE() : new ZERO(),
+            CFUN cf => cf.f(c) switch
+            {
+                true => new ONE(),
+                false => new ZERO()
+            },
+            ALL => der(c, new CFUN()),
+            _ => throw new System.Exception("GOT UNKNOWN REXP")
+        };
+
+        public Rexp ders(string s, Rexp r) 
+        {
+            switch (s.Length)
+            {
+                case 0:
+                    return r;
+                default:
+                {
+                    char c = s[0];
+                    s = s.Substring(1);
+                    return ders(s, simp(der(c, r)));
+                }
+            }
+        }
+
+        public Rexp simp(Rexp r)
+        {
+            switch (r)
+            {
+                case ALT alt:
+                    {
+                        Rexp r1s = simp(alt.r1);
+                        Rexp r2s = simp(alt.r2);
+                        switch(r1s, r2s)
+                        {
+                            case (ZERO, _):
+                                return r2s;
+                            case (_, ZERO):
+                                return r1s;
+                            case (_, _):
+                                if (r1s == r2s)
+                                    return r1s;
+                                else
+                                    return new ALT(r1s, r2s);
+                        }
+                    }
+                case SEQ seq:
+                    {
+                        Rexp r1s = simp(seq.r1);
+                        Rexp r2s = simp(seq.r2);
+                        switch (r1s, r2s)
+                        {
+                            case (ZERO, _):
+                                return new ZERO();
+                            case (_, ZERO):
+                                return new ZERO();
+                            case (ONE, _):
+                                return r2s;
+                            case (_, ONE):
+                                return r1s;
+                            case (_, _):
+                                return new SEQ(r1s, r2s);
+                        }
+                    }
+                default:
+                    return r;
+            }
+        }
+
+        public bool Match(Rexp r, string s)
+        {
+            return nullable(ders(s, r));
+        }
+
+        public int Size(Rexp r) => r switch
+        {
+            ZERO _ => 1,
+            ONE _ => 1,
+            CHAR _ => 1,
+            ALT alt => 1 + Size(alt.r1) + Size(alt.r2),
+            SEQ seq => 1 + Size(seq.r1) + Size(seq.r2),
+            STAR star => 1 + Size(star.r)
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using ConsoleTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace cw1
+{
+    class Program
+    {
+        private static readonly Matcher matcher = new Matcher();
+
+        private static Dictionary<string, bool> RunTests(Rexp r, string[] testCases)
+        {
+            var results = new Dictionary<string, bool>();
+
+            foreach (string t in testCases)
+            {
+                results.Add(t, matcher.Match(r, t));
+            }
+
+            return results;
+        }
+
+        private static void Question3()
+        {
+            string[] testCases = new string[]
+            {
+                "",
+                "a",
+                "aa",
+                "aaa",
+                "aaaaa",
+                "aaaaaa",
+            };
+
+            Rexp r1 = new OPTIONAL(new CHAR('a'));
+            var r1Results = RunTests(r1, testCases);
+
+            Rexp r2 = new NOT(new CHAR('a'));
+            var r2Results = RunTests(r2, testCases);
+
+            Rexp r3 = new NTIMES(new CHAR('a'), 3);
+            var r3Results = RunTests(r3, testCases);
+
+            Rexp r4 = new NTIMES(new OPTIONAL(new CHAR('a')), 3);
+            var r4Results = RunTests(r4, testCases);
+
+            Rexp r5 = new UPTO(new CHAR('a'), 3);
+            var r5Results = RunTests(r5, testCases);
+
+            Rexp r6 = new UPTO(new OPTIONAL(new CHAR('a')), 3);
+            var r6Results = RunTests(r6, testCases);
+
+            Rexp r7 = new BETWEEN(new CHAR('a'), 3, 5);
+            var r7Results = RunTests(r7, testCases);
+
+            Rexp r8 = new BETWEEN(new OPTIONAL(new CHAR('a')), 3, 5);
+            var r8Results = RunTests(r8, testCases);
+
+            Rexp cornerCase = new NTIMES(new CHAR('a'), 0);
+            var ccResults = RunTests(cornerCase, testCases);
+
+            var resultsTable = new ConsoleTable("string", "a?", "~a", "a{3}", "(a?){3}", "a{..3}", "(a?){..3}", "a{3..5}", "(a?){3..5}", "a{0}");
+            foreach (string tcase in testCases)
+            {
+                resultsTable.AddRow(
+                    tcase,
+                    r1Results[tcase],
+                    r2Results[tcase],
+                    r3Results[tcase],
+                    r4Results[tcase],
+                    r5Results[tcase],
+                    r6Results[tcase],
+                    r7Results[tcase],
+                    r8Results[tcase],
+                    ccResults[tcase]
+                );
+            }
+            resultsTable.Write();
+
+        }
+        private static void Question5()
+        {
+
+            HashSet<char> set1 = "abcdefghijklmnopqrstuvwxyz0123456789_.-".ToHashSet();
+            HashSet<char> set2 = "abcdefghijklmnopqrstuvwxyz0123456789.-".ToHashSet();
+            HashSet<char> set3 = "abcdefghijklmnopqrstuvwxyz.".ToHashSet();
+
+            Rexp addr = new SEQ(new PLUS(new RANGE(set1)), new CHAR('@'));
+            Rexp domain = new SEQ(new PLUS(new RANGE(set2)), new CHAR('.'));
+            Rexp topLevelDomain = new BETWEEN(new RANGE(set3), 2, 6);
+            Rexp emailAddr = new SEQ(new SEQ(addr, domain), topLevelDomain);
+
+            string input = "james.legge@kcl.ac.uk";
+            bool result = matcher.Match(matcher.simp(emailAddr), input);
+            Console.WriteLine($"Is \"{input}\" an email address?: {result}");
+        }
+
+        private static void Question6()
+        {
+            //        \/\*(?!.*\*\/.*)\*\/
+
+            Rexp innerNot =  new NOT(new SEQ(new SEQ(new SEQ(new STAR(new ALL()), new CHAR('*')), new CHAR('/')), new STAR(new ALL())));
+            Rexp r = new SEQ(new SEQ ( new SEQ(     new SEQ     (new CHAR('/'), new CHAR('*'))  , innerNot),     new CHAR('*')), new CHAR('/'));
+
+            string[] testCases = new string[]
+            {
+                "/**/",
+                "/*foobar*/",
+                "/*test*/test*/",
+                "/*test/*test*/"
+            };
+
+            foreach (string tCase in testCases)
+            {
+                bool res = matcher.Match(r, tCase);
+                Console.WriteLine($"{tCase}: {res}");
+            }
+        }
+
+        private static void Question7()
+        {
+            Rexp r1 = new SEQ(new SEQ(new CHAR('a'), new CHAR('a')), new CHAR('a'));
+            Rexp r2 = new SEQ(new BETWEEN(new CHAR('a'), 19, 19), new OPTIONAL(new CHAR('a')));
+
+            Rexp r4 = new PLUS(new PLUS(r1));
+            Rexp r5 = new PLUS(new PLUS(r2));
+
+            string[] testCases = new string[]
+            {
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            };
+            
+            Console.WriteLine("Testing: r1");
+            foreach (string tcase in testCases)
+            {
+                Console.WriteLine(matcher.Match(r1, tcase));
+            }
+
+            Console.WriteLine("\nTesting: r2");
+            foreach (string tcase in testCases)
+            {
+                Console.WriteLine(matcher.Match(r2, tcase));
+            }
+            
+            Console.WriteLine("\nTesting: (r1+)+");
+            foreach (string tcase in testCases)
+            {
+                Console.WriteLine(matcher.Match(r4, tcase));
+            }
+            
+            Console.WriteLine("\nTesting: (r2+)+");
+            foreach (string tcase in testCases)
+            {
+                Console.WriteLine(matcher.Match(r5, tcase));
+            }
+            
+        }
+
+        static void Main()
+        { 
+            
+            Console.WriteLine("\nQUESTION 3:");
+            Question3();
+
+            Console.WriteLine("\nQUESTION 5:");
+            Question5();
+
+            Console.WriteLine("\nQUESTION 6:");
+            Question6();
+            
+            Console.WriteLine("\nQUESTION 7:");
+            Question7();
+        }
+    }
+}
